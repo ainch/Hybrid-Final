5586f6ccf377418b9092616ca8b3f1d70c936aca
ainch <chldbsdu1001@naver.com>
2022-01-04 22:54:59 +0900

    make script replace global variables with extern global variables

--- scripts/main.py
+++ scripts/main.py
@@ -1,9 +1,12 @@
 import parse
+import refactor
 
 if __name__=="__main__":
     all_files = parse.get_all_files()
-    print(all_files)
     total_global_variables = parse.get_global_variables(all_files)
  
-    for global_variable in total_global_variables:
-        print('[',global_variable,']')
+    with open('out.txt', 'w') as f:
+        for global_variable in total_global_variables:
+            f.write(global_variable)
+
+    refactor.refactor()
--- scripts/refactor.py
+++ scripts/refactor.py
@@ -1,14 +1,6 @@
-import os
+import parse
 
-def get_files(path_dir):
-    list_dir = os.listdir(path_dir)
-    list_paths_file = [ path_dir + '/' + path_file for path_file in list_dir ]
-    return list_paths_file
-
-def get_all_files():
-    return get_files('inc') + get_files('src')
-
-def extract_global_variables(path_file):
+def change_global_to_extern(path_file):
     lines = []
     with open(path_file) as f:
         while True:
@@ -17,38 +9,39 @@ def extract_global_variables(path_file):
                 lines.append(line)
             else:
                 break
-
     num_parentheses = 0
     num_brace = 0
     num_inequality = 0
 
-    global_variables = []
+    lines_new = []
 
     for line in lines:
+        line_new = line
         if '(' in line:
             num_parentheses = num_parentheses + 1
         if '{' in line:
             num_brace = num_brace + 1
         if '<' in line:
             num_inequality = num_inequality + 1
-            
         if num_parentheses == 0 and num_brace == 0 and num_inequality == 0:
             if '#' not in line and 'extern' not in line and line[0] != '/' \
                 and line != '' and line != '\t' \
                 and '*/' not in line and '/*' not in line \
                 and 'typedef' not in line and 'namespace' not in line:
-                global_variables.append(line)
-
+                line_new = 'extern' + ' ' + line
         if ')' in line:
             num_parentheses = num_parentheses - 1
         if '}' in line:
             num_brace = num_brace - 1
         if '>' in line:
             num_inequality = num_inequality - 1
-    return global_variables
+        lines_new.append(line_new)
 
-def get_global_variables(all_files):
-    total_global_variables = []
+    with open(path_file, 'w') as f:
+        for line_new in lines_new:
+            f.write(line_new)
+
+def refactor():
+    all_files = parse.get_all_files()
     for path_file in all_files:
-        total_global_variables = total_global_variables + extract_global_variables(path_file)
-    return total_global_variables
+        change_global_to_extern(path_file)
